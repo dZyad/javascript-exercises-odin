352b4f11d51226d8b86e15b6f2dc7bb8da1aaf0e
dand <79340018+dZyad@users.noreply.github.com>
2024-01-08 10:59:20 -0300

    05_sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (!isNumber(num1) || !isNumber(num2) || num1 < 0 || num2 < 0 ) return "ERROR";
+    let lowerLimit = Math.min(num1, num2);
+    let upperLimit = Math.max(num1, num2);
+    let result = 0;
 
+    for (let i = lowerLimit; i <= upperLimit ; i++ ) {
+        result += i;
+    }
+    return result;
 };
+
+function isNumber(value) {
+    return typeof value === 'number';
+}
 
 // Do not edit below this line
 module.exports = sumAll;
