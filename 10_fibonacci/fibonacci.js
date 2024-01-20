ad91f61b0970d31ae39214145f4b3c6940b2d283
dand <79340018+dZyad@users.noreply.github.com>
2024-01-19 23:28:00 -0300

    10_fibonacci done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(value) {
+    //(1, 1, 2, 3)
+    if ( +value === 0 ) return 0;
+    if ( value > 0 ) {
+        const sequence = [1, 1]
+        for ( let i = 2 ; i < value ; i++ ) {
+            let last = sequence[ i - 1];
+            let prev = sequence[ i - 2];
+            sequence.push( last + prev );
+        }
+        return sequence[value - 1];
+    }
+    return "OOPS";
 };
 
 // Do not edit below this line
