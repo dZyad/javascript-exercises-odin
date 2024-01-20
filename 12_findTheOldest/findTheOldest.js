ad3bea6a142936eb7ba2364778da2d1823e0dbc7
dand <79340018+dZyad@users.noreply.github.com>
2024-01-20 00:35:30 -0300

    12-findOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const sortedPeople = people.sort((a, b) => {
+        const lastGuy = lastDate(a) - a.yearOfBirth;
+        const nextGuy = lastDate(b) - b.yearOfBirth;
+        return nextGuy - lastGuy;
+    });
+    return sortedPeople[0];
 };
+
+function lastDate(person) {
+    const currentYear = (new Date()).getFullYear();
+    return Object.hasOwn(person, 'yearOfDeath') ? person.yearOfDeath : currentYear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
